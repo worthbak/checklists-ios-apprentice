94d7b719fd9963d1c23040bf5db52e2f94e73d97
Worth Baker <wbaker@greenmtd.com>
2014-12-07 14:33:47 -0500

    ChecklistTableView filled with dummy data

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -24,11 +24,26 @@ class ChecklistViewController: UITableViewController {
   // MARK: - Table View Methods
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    return 100
   }
   
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
     let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem") as UITableViewCell
+    
+    let label = cell.viewWithTag(1000) as UILabel
+    
+    if indexPath.row % 5 == 0 {
+      label.text = "Walk the dog"
+    } else if indexPath.row % 5 == 1 {
+      label.text = "Brush my teeth"
+    } else if indexPath.row % 5 == 2 {
+      label.text = "Learn iOS development"
+    } else if indexPath.row % 5 == 3 {
+      label.text = "Soccer practice"
+    } else if indexPath.row % 5 == 4 {
+      label.text = "Eat ice cream"
+    }
     
     return cell
   }
