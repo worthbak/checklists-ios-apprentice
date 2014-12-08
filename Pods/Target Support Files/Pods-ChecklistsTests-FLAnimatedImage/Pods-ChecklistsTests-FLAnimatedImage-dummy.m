1a9996520911baa2793214de37d89b11872dd22f
Worth Baker <wbaker@greenmtd.com>
2014-12-08 15:55:45 -0500

    Adds Cocoapods, FLAnimatedImage (real commit)

---
+++ Pods/Target Support Files/Pods-ChecklistsTests-FLAnimatedImage/Pods-ChecklistsTests-FLAnimatedImage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChecklistsTests_FLAnimatedImage : NSObject
+@end
+@implementation PodsDummy_Pods_ChecklistsTests_FLAnimatedImage
+@end
---
+++ Pods/Target Support Files/Pods-ChecklistsTests/Pods-ChecklistsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChecklistsTests : NSObject
+@end
+@implementation PodsDummy_Pods_ChecklistsTests
+@end
