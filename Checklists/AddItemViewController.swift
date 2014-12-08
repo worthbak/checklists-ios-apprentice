6614313a43ef883ec55f0d834f594e9a978e62b5
Worth Baker <wbaker@greenmtd.com>
2014-12-08 08:53:42 -0500

    Enhances functionality of the add item view: done button enabler, text field delegate etc :D

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
   
   @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
 
   @IBAction func done(sender: AnyObject) {
     println("Contents of the text field are: \(textField.text)")
@@ -21,12 +22,30 @@ class AddItemViewController: UITableViewController {
     dismissViewControllerAnimated(true, completion: nil)
   }
   
+  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    let oldText: NSString = textField.text
+    let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
+    
+    if newText.length > 0 {
+    doneBarButton.enabled = true
+    } else {
+      doneBarButton.enabled = false
+    }
+    
+    return true
+  }
+  
   override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
     return nil
   }
   
   override func viewDidLoad() {
     tableView.rowHeight = 44
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
   }
 
 }
