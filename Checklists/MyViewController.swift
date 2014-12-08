a1e1211d889918571f84b51b661feb9b081a5bf0
Worth Baker <wbaker@greenmtd.com>
2014-12-08 15:48:38 -0500

    Updates new view title

--- Checklists/MyViewController.swift
+++ Checklists/MyViewController.swift
@@ -14,6 +14,8 @@ class MyViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+      
+      title = "Party Screen!" 
     }
 
     override func didReceiveMemoryWarning() {
